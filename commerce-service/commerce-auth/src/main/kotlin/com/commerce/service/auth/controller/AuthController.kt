436c262968489e5069af31ad76efaadc88aef541
이호준 <psh10066@gmail.com>
2024-08-20 21:19:01 +0900

    멀티모듈 패키지 예제 Service 의존 수정

--- commerce-service/commerce-auth/src/main/kotlin/com/commerce/service/auth/controller/AuthController.kt
+++ commerce-service/commerce-auth/src/main/kotlin/com/commerce/service/auth/controller/AuthController.kt
@@ -1,16 +1,16 @@
 package com.commerce.service.auth.controller
 
-import com.commerce.service.auth.application.service.AuthService
+import com.commerce.service.auth.application.usecase.AuthUseCase
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AuthController(
-    private val authService: AuthService
+    private val authUseCase: AuthUseCase
 ) {
 
     @GetMapping("/auth/hello")
     fun hello(): String {
-        return authService.findMember(1).toString()
+        return authUseCase.findMember(1).toString()
     }
 }
