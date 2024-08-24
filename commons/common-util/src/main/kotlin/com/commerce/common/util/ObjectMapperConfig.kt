2c046c899a42b4bd58fef733dcb323fbfe2766c8
이호준 <psh10066@gmail.com>
2024-08-24 15:26:48 +0900

    ObjectMapper Kotlin Module 추가

---
+++ commons/common-util/src/main/kotlin/com/commerce/common/util/ObjectMapperConfig.kt
@@ -0,0 +1,26 @@
+package com.commerce.common.util
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class ObjectMapperConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val objectMapper = ObjectMapper()
+        objectMapper
+            .registerModule(KotlinModule.Builder()
+                .withReflectionCacheSize(512)
+                .configure(KotlinFeature.NullToEmptyCollection, false)
+                .configure(KotlinFeature.NullToEmptyMap, false)
+                .configure(KotlinFeature.NullIsSameAsDefault, false)
+                .configure(KotlinFeature.SingletonSupport, false)
+                .configure(KotlinFeature.StrictNullChecks, false)
+                .build())
+        return objectMapper
+    }
+}
