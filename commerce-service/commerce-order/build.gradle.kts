70aa618d41b6f006a0c9ca1d0abc5a5b9a36d683
hh.jang <hyunho.jang.dev@gmail.com>
2024-08-31 17:01:58 +0900

    #7 주문 서버 준비

---
+++ commerce-service/commerce-order/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation(project(":commons:common-util"))
+    implementation(project(":commons:model"))
+    implementation(project(":commons:persistence-database"))
+}
---
+++ commerce-service/commerce-order/src/main/kotlin/com/commerce/service/order/OrderConfiguration.kt
@@ -0,0 +1,7 @@
+package com.commerce.service.order
+
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan("com.commerce.common")
+@ComponentScan
+class OrderConfiguration
