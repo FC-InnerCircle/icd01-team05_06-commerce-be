7682f4bce1c63c03f589ea520f942e497957de75
JaeHunJaeHun <jhl9838@gmail.com>
2024-09-02 14:35:11 +0900

    #3 BaseEntity 추가

---
+++ commons/persistence-database/src/main/kotlin/com/commerce/common/persistence/BaseEntity.kt
@@ -0,0 +1,18 @@
+package com.commerce.common.persistence
+
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity : BaseTimeEntity() {
+
+    @CreatedBy
+    var createdBy: String? = null
+
+    @LastModifiedBy
+    var lastModifiedBy: String? = null
+}
