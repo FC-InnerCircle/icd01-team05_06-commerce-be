3ce88bdd74b992943a6f1df06ddf4c9b454182f6
JaeHunJaeHun <jhl9838@gmail.com>
2024-09-07 09:54:08 +0900

    #3 BasePageRequest 클래스 생성

---
+++ commerce-service/commerce-product/src/main/kotlin/com/commerce/service/product/controller/request/BasePageRequest.kt
@@ -0,0 +1,6 @@
+package com.commerce.service.product.controller.request
+
+open class BasePageRequest (
+    val page: Int = 0,
+    val size: Int = 20,
+)
--- commerce-service/commerce-product/src/main/kotlin/com/commerce/service/product/controller/request/ProductQueryRequest.kt
+++ commerce-service/commerce-product/src/main/kotlin/com/commerce/service/product/controller/request/ProductQueryRequest.kt
@@ -1,18 +1,15 @@
 package com.commerce.service.product.controller.request
 
 import com.commerce.service.product.application.usecase.query.SelectQuery
-import org.springframework.data.domain.PageRequest
 
 data class ProductQueryRequest(
     val productCategoryId: Long,
-    val page: Int = 0,
-    val size: Int = 20,
-) {
+) : BasePageRequest(){
     fun toQuery(): SelectQuery {
         return SelectQuery(
             categoryId = productCategoryId,
-            page = page,
-            size = size,
+            page = this.page,
+            size = this.size,
         )
     }
 }
