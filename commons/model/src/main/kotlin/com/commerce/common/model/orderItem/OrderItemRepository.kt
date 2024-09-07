9948e4d7655ebaf126ad428c5cc3950857369a7b
hh.jang <fallear1@gmail.com>
2024-09-07 13:29:53 +0900

    #7 #11 삭제

--- commons/model/src/main/kotlin/com/commerce/common/model/orderItem/OrderItemRepository.kt
+++
@@ -1,4 +0,0 @@
-package com.commerce.common.model.orderItem
-
-interface OrderItemRepository {
-}
--- commons/persistence-database/src/main/kotlin/com/commerce/common/persistence/orderItem/OrderItemJpaRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.commerce.common.persistence.orderItem
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface OrderItemJpaRepository : JpaRepository<OrderItemJpaEntity, Long>
