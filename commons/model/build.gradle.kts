90900fd7e6749a5bede4e6b1ce986fa39f5ef430
hh.jang <fallear1@gmail.com>
2024-09-07 15:17:28 +0900

    #7 의존성 변경

--- commons/model/build.gradle.kts
+++ commons/model/build.gradle.kts
@@ -1,3 +1,3 @@
 dependencies {
-    api("org.springframework.boot:spring-boot-starter-data-jpa") // Pageable 가져오기 위한 의존성 추가
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa") // Pageable 가져오기 위한 의존성 추가
 }
