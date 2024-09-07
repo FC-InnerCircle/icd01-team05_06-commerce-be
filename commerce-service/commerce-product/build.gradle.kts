dbcb04112b1074f917dca134d897cae558840d6c
JaeHunJaeHun <jhl9838@gmail.com>
2024-09-08 03:03:50 +0900

    #3 build.gradle.kts ascidoctor 설정 추가

--- commerce-service/commerce-product/build.gradle.kts
+++ commerce-service/commerce-product/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
@@ -6,4 +10,36 @@ dependencies {
     implementation(project(":commons:persistence-database"))
 
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+val asciidoctorExt: Configuration by configurations.creating
+dependencies {
+    asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor")
+}
+
+val snippetsDir by extra { file("build/generated-snippets") }
+
+tasks {
+    test {
+        outputs.dir(snippetsDir)
+    }
+
+    asciidoctor {
+        inputs.dir(snippetsDir)
+        configurations(asciidoctorExt.name)
+        baseDirFollowsSourceFile()
+        dependsOn(test)
+        doLast {
+            copy {
+                from("build/docs/asciidoc")
+                into("build/resources/main/static/docs")
+            }
+        }
+    }
+
+    bootJar {
+        dependsOn(asciidoctor)
+    }
 }
