02f153edcb5613db591b2bd4355f71453c81fccd
이호준 <psh10066@gmail.com>
2024-09-23 20:42:26 +0900

    #11 Local 환경 삭제

--- commerce-service/commerce-order/src/main/kotlin/com/commerce/service/order/config/JasyptConfig.kt
+++ commerce-service/commerce-order/src/main/kotlin/com/commerce/service/order/config/JasyptConfig.kt
@@ -5,13 +5,11 @@ import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
 import org.jasypt.iv.RandomIvGenerator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 
 // JasyptConfig 클래스
 // Jasypt 라이브러리를 사용하여 암호화를 위한 설정을 하는 클래스
 // AWS 상용 데이터베이스 정보를 암호화하여 사용하기 위해 사용
 @Configuration
-@Profile("!local")
 class JasyptConfig {
     @Bean("jasyptStringEncryptor")
     fun stringEncryptor(): StringEncryptor {
